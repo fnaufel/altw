ce39ed8af2cfedfcad4218186366c1afddca368e
Fernando Naufel <fnaufel@gmail.com>
2022-04-05 17:23:31 -0300

    Deactivate selection after copying

--- R/altw.R
+++ R/altw.R
@@ -4,14 +4,14 @@
 #'
 #' @author fnaufel
 #' @export
-#' @importFrom rstudioapi getSourceEditorContext
+#' @importFrom rstudioapi getActiveDocumentContext setSelectionRanges
 #' @importFrom clipr write_clip
 #'
 altw <- function() {
 
-  context <- rstudioapi::getSourceEditorContext()
+  context <- rstudioapi::getActiveDocumentContext()
 
-  if (!empty_selection(context)) {
+  if (!is.null(context) && !empty_selection(context)) {
 
     # Collect selection(s)
     my_text <- mapply(
@@ -21,6 +21,11 @@ altw <- function() {
     )
 
     clipr::write_clip(my_text)
+
+    # Deactivate selection, leaving cursor at end
+    rstudioapi::setSelectionRanges(
+      context$selection[[1]]$range$end
+    )
 
   }
 
