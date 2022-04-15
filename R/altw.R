53512636f2d9bdbccdab8a776c56cd87f351899a
Fernando Naufel <fnaufel@gmail.com>
2022-04-15 19:33:51 -0300

    Do not test for empty selection anymore

--- R/altw.R
+++ R/altw.R
@@ -11,22 +11,18 @@ altw <- function() {
 
   context <- rstudioapi::getSourceEditorContext()
 
-  if (!is.null(context) && !empty_selection(context)) {
+  # Collect selection(s)
+  my_text <- mapply(
+    function(x) { paste0(x$text) },
+    context$selection,
+    USE.NAMES = FALSE
+  )
 
-    # Collect selection(s)
-    my_text <- mapply(
-      function(x) { x$text },
-      context$selection,
-      USE.NAMES = FALSE
-    )
+  clipr::write_clip(my_text)
 
-    clipr::write_clip(my_text)
-
-    # Deactivate selection by inserting selected text over itself (!)
-    # This was the only way I found to do it :-(
-    rstudioapi::insertText(my_text)
-
-  }
+  # Deactivate selection by inserting selected text over itself (!)
+  # This was the only way I found to do it :-(
+  rstudioapi::insertText(my_text)
 
 }
 
